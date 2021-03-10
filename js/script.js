6efb8b4b3a919adc0f64d587cee71c75f7f02e3b
Gabriele Alessi <89archaon89@gmail.com>
2021-03-10 18:53:45 +0100

    Esercizio con html e css base

---
+++ js/script.js
@@ -0,0 +1,50 @@
+var generaBtn = document.getElementById("genera");
+var annullaBtn = document.getElementById("annulla");
+var costo = 0.21;
+var costoTotale = 0;
+
+generaBtn.addEventListener('click', function(){
+
+    var name = document.getElementById('name').value;
+    var km = parseInt(document.getElementById('km').value);
+    var age = document.getElementById('age').value;
+
+    console.log(name, km, age);
+
+if (name !== " " && km > 0 && age !== " ") {
+
+    costoTotale = costo * km;
+    document.getElementById('nome').innerHTML = name;
+
+    var rndCarrozza = Math.floor(Math.random() * 10) + 1;
+    document.getElementById('carrozza').innerHTML = rndCarrozza;
+
+    var rndCodice = Math.floor(Math.random() * (100000-90000)) + 90000;
+    document.getElementById('codice').innerHTML = rndCodice;
+
+
+
+    if(age == 1){ //minorenne
+        var costoEffettivo = ((costoTotale * 80) / 100).toFixed(2);
+        document.getElementById('offerta').innerHTML = "Offerta Minorenne";
+        document.getElementById('costoEffettivo').innerHTML = costoEffettivo;
+    }else if(age == 3){
+        var costoEffettivo = ((costoTotale * 60) / 100).toFixed(2);
+        document.getElementById('offerta').innerHTML = "Offerta Over 65";
+        document.getElementById('costoEffettivo').innerHTML = costoEffettivo;
+
+    }else{
+        var costoEffettivo = costoTotale;
+        document.getElementById('offerta').innerHTML = "---";
+        document.getElementById('costoEffettivo').innerHTML = costoEffettivo;
+
+    }
+    console.log(costoEffettivo);
+} else {
+ alert("Si prega di inserire tutti i dati");
+}
+})
+
+annullaBtn.addEventListener('click', function(){
+
+})
